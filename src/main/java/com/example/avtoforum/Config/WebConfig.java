e3f8cb4c64e231934d9d3e80a78fe054cf200a0a
Tebriz Memmedov <tabriz.mammadov2022@khazar.org>
2025-06-10 18:36:57 +0400

    Update WebConfig.java

--- src/main/java/com/example/avtoforum/Config/WebConfig.java
+++ src/main/java/com/example/avtoforum/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://165.227.151.218"))
+                .allowedOrigins("http://localhost:3000", "http://165.227.151.218")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
